1b774ec6ed1dd7fef213358af7304d32a52be459
Gabriel BRUN <lydstyl@gmail.com>
2018-10-16 12:27:05 +0200

    extractionFiltresSysObj.txt ok

--- index.js
+++ index.js
@@ -2,24 +2,43 @@ const fs = require('fs');
 const convert = require('xml-js');
  
 const xml = fs.readFileSync('1016-nl-sys-obj-filtres-group.xml', 'utf8');
+const lang = 'fr-FR'
+
+let txt = '';
 let res = JSON.parse(convert.xml2json(xml, {compact: false, spaces: 4}));
 res = res.elements[0].elements
+
+function tab(nb) {
+    let str = '';
+    for (let i = 0; i < nb; i++) {
+        const element = nb     
+        str += '\t';
+    }
+    return str
+}
 
 res.forEach(el => {
     el.elements.forEach(el =>{
         if (el.name == 'attribute-definition') {
             if(el.attributes['attribute-id'].includes('Filtre')){
-                console.log(el.attributes['attribute-id']);
+                txt += '\n' + el.attributes['attribute-id'] + '\n';
                 el.elements.forEach(el => {
-                    if (el.name == 'display-name') {
+                    if (el.name == 'value-definitions') {
                         el.elements.forEach(el => {
-                            console.log('\t' + el.text);
-                            
+                            el.elements.forEach(el =>{
+                                if (el.name == 'display') {
+                                    txt += tab(1) + el.attributes['xml:lang'] + '\n';
+                                    el.elements.forEach(el => {
+                                        txt += tab(2) + el.text + '\n' + '\n';
+                                    })
+                                }
+                            })
                         })
                     }
-                    
                 });
             }
         }
     });
 });
+
+fs.writeFileSync('result.txt', txt, {encoding: 'utf8'})
