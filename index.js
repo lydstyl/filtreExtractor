cfdb8593984775cdadbcff21fced8ef5b376ef15
Gabriel BRUN <lydstyl@gmail.com>
2018-10-16 16:01:49 +0200

    refinement-definitions ok

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const convert = require('xml-js');
  
-const xml = fs.readFileSync('1016-nl-sys-obj-filtres-group.xml', 'utf8');
-const lang = 'fr-FR'
+const xml = fs.readFileSync('test.xml', 'utf8');
+// const lang = 'fr-FR'
 
 let txt = '';
 let res = JSON.parse(convert.xml2json(xml, {compact: false, spaces: 4}));
-res = res.elements[0].elements
+res = res.elements[0].elements;
 
 function tab(nb) {
     let str = '';
@@ -18,27 +18,33 @@ function tab(nb) {
 }
 
 res.forEach(el => {
-    el.elements.forEach(el =>{
-        if (el.name == 'attribute-definition') {
-            if(el.attributes['attribute-id'].includes('Filtre')){
-                txt += '\n' + el.attributes['attribute-id'] + '\n';
+    if (el.name == 'category') {
+        txt += '\n' + el.attributes['category-id'] + '\n';
+        el.elements.forEach(el => {
+            if (el.name == 'refinement-definitions') {
                 el.elements.forEach(el => {
-                    if (el.name == 'value-definitions') {
-                        el.elements.forEach(el => {
-                            el.elements.forEach(el =>{
-                                if (el.name == 'display') {
-                                    txt += tab(1) + el.attributes['xml:lang'] + '\n';
-                                    el.elements.forEach(el => {
-                                        txt += tab(2) + el.text + '\n' + '\n';
-                                    })
-                                }
-                            })
-                        })
+                    if (el.name == 'refinement-definition') {
+                        if (el.attributes['attribute-id']) {
+                            if (el.attributes['attribute-id'].includes('Filtre')) {
+                                txt += tab(1) + el.attributes['attribute-id'] + '\n';
+                                el.elements.forEach(el => {
+                                    if (el.name = 'display-name') {
+                                        if (el.attributes) {
+                                            if (el.attributes['xml:lang']) {
+                                                txt += tab(2) + el.attributes['xml:lang'] + '\n';
+                                                txt += tab(3) + el.elements[0].text + '\n';
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        }
                     }
                 });
             }
-        }
-    });
+        });
+    }
 });
 
+console.log(txt);
 fs.writeFileSync('result.txt', txt, {encoding: 'utf8'})
